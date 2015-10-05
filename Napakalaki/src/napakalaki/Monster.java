b2a630eec4da8439dc6fa1c7669d0caee5aa6ba8
durenot <durenot@172.20.97.69>
2015-10-05 10:22:34 +0200

    Fin P1

--- Napakalaki/src/napakalaki/Monster.java
+++ Napakalaki/src/napakalaki/Monster.java
@@ -10,5 +10,28 @@ package napakalaki;
  * @author durenot
  */
 public class Monster {
+    private String name;
+    private int combatLevel;
+    private BadConsequence bc;
+    private Prize price;
     
+    Monster(String name, int level, BadConsequence bc, Prize price){
+        this.name=name;
+        this.combatLevel=level;
+        this.bc=bc;
+        this.price=price;
+    }
+   
+    public int getCombatLevel(){
+        return this.combatLevel;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public BadConsequence getBc(){
+        return this.bc;
+    }
+    public Prize getPrize(){
+        return this.price;
+    }
 }
